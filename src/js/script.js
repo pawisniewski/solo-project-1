5e342731d6574afb4a063445d096a5fabfc56764
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-01-16 15:30:55 +0100

    Add base HTML code for other subpages and 'sectionClickHandler' function in JS. Reorganize structure of directories and SCSS files

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,36 @@ document.querySelector('.hamburger').addEventListener('click', function(e) {
   e.preventDefault();
   toggleMenu();
 });
+
+
+const sectionClickHandler = function(event) {
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
+
+  const activeLinks = document.querySelectorAll('.sidebar-menu a .active');
+  for (let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  clickedElement.classList.add('active');
+
+  const activeSections = document.querySelectorAll('.content .active');
+  for (let activeSection of activeSections){
+    activeSection.classList.remove('active');
+  }
+
+  const sectionSelector = clickedElement.getAttribute('href');
+  console.log('sectionSelector:', sectionSelector);
+
+  const targetSection = document.querySelector(sectionSelector);
+  console.log('targetSection:', targetSection);
+
+  targetSection.classList.add('active');
+};
+
+const links = document.querySelectorAll('.sidebar-menu a');
+
+for(let link of links){
+  link.addEventListener('click', sectionClickHandler);
+}
